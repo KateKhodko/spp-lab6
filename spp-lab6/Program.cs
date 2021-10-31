ace759e75b10080ca029273f272fec07df7c533d
KateKhodko <kottikate@gmail.com>
2021-10-31 12:54:35 +0300

    Initial commit

---
+++ spp-lab6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace spp_lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logBuffer = new LogBuffer("C:\\developing\\projects\\С#\\spp-lab6\\spp-lab6\\test.txt");
+            for (var i = 1; i <= 100; i++)
+            {
+                logBuffer.Add(i.ToString());
+            }
+            Console.WriteLine("Press the Enter key to exit ... ");
+            Console.ReadLine();
+            logBuffer.Close();
+        }
+    }
+}
